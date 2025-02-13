e72a15d057f71c4594d86e00b18d7bf06d9a6c0e
Yves Matanga <yves.matanga.dev@gmail.com>
2025-02-13 21:59:16 +0200

    Backend -- front-end complete with random image selection
    Front End + Backend complete with random image selection

--- web-back-end/flask/api/main.py
+++ web-back-end/flask/api/main.py
@@ -1,14 +1,17 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, send_from_directory
 from flask_cors import CORS
 #from flask import current_app as app
 from flask.json import JSONEncoder
 import decimal
-
+import os
+import pandas as pd
+import random
 
 app = Flask(__name__)
 CORS(app)
 
-
+UPLOAD_FOLDER = os.path.abspath(os.path.join(os.getcwd(), "../../sample_images"))
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 #modifing JSON Encoder
 class JsonEncoder(JSONEncoder):
@@ -21,11 +24,35 @@ class JsonEncoder(JSONEncoder):
 @app.route('/api/mmsys/query', methods=['GET'])
 def fect_test_response():
     desc = request.args.get('desc') #get query description    return 'flask_api ok'
+    k = int(request.args.get('k')) #kumber of retrievals
+    print("K:",k)
     message = desc.upper()
-    response_data = {
-        "message": message,
-    }
-    return jsonify(response_data)
+
+    img_df = pd.read_csv('../../sample_images/sample_images.csv')
+    Ns = len(img_df)
+    sample_ids = random.sample(range(0,Ns-1),k)
+    sample_df = img_df.iloc[sample_ids,:]#get sample image subset
+
+    image_list = sample_df.file_name.tolist()
+    available_images = os.listdir(app.config["UPLOAD_FOLDER"])#read all available images
+    filtered_images = [img for img in available_images if img in image_list]
+    #generate url for filter images
+    image_urls = [f"http://127.0.0.1:5002/api/image/{img}" for img in filtered_images]
+    
+    response_list = []
+    for i in range(len(image_urls)):
+        img = {'img_url':image_urls[i], 'score':round(random.uniform(0,1),3)}
+        response_list.append(img)
+
+    #print(response_list)
+    return jsonify(response_list)
+
+@app.route("/api/image/<filename>")
+def serve_image(filename):
+    """
+    Serves an image from the uploads folder only if it is in ALLOWED_IMAGES.
+    """
+    return send_from_directory(app.config["UPLOAD_FOLDER"], filename)
 
 
 if __name__ == '__main__':
--- web-front-end/src/components/AnalyticsForm.js
+++ web-front-end/src/components/AnalyticsForm.js
@@ -1,44 +1,87 @@
 
-import { Button, Form, Input } from 'antd';
+import { Button, Form, Input} from 'antd';
 import React, { useState } from 'react';
+import { InputNumber } from "antd";
+import face from '../img/face.jpeg'
+import {Layout } from 'antd';
+const { Header, Footer, Sider, Content } = Layout;
+
+const siderStyle = {
+  /*textAlign: 'center',
+  lineHeight: '120px',
+  color: '#fff',*/
+  backgroundColor: '#FFFFFF',
+};
+
 
 const layout = {
     labelCol: {
       span: 8,
     },
     wrapperCol: {
-      span: 16,
+      span: 26,
     },
   };
-  
+
+
+// Define the original object
+const originalItem = {
+  img_url: face,
+  score: 0.9,
+};
+// Create an array with 10 copies of the original object
+const imageArray = Array.from({ length: 20 }, () => ({ ...originalItem }));
+const chunkArray = (array, chunkSize) => {
+  const chunks = [];
+  for (let i = 0; i < array.length; i += chunkSize) {
+    chunks.push(array.slice(i, i + chunkSize));
+  }
+  return chunks;
+};
+ // Chunk the imageArray into sub-arrays of 5 images each
+const chunkedImages = chunkArray(imageArray, 3);
+
 const AnalyticsForm = ()=>{
 
     const [desc,setDesc] = useState('');
+    const [k,setK] = useState(6);
     const [response, setResponse] = useState(null);
-  
+    const [rImageArray, setRImageArray] = useState(chunkedImages);
+   
     const handleSubmit = (e) => {
       {/*e.preventDefault(); // Prevent form from refreshing the page*/}
       // Send GET request to Flask API with query parameters from form inputs
-      const apiUrl = "http://127.0.0.1:5002/api/mmsys/query?desc="+desc;
+      const apiUrl = "http://127.0.0.1:5002/api/mmsys/query?desc="+desc+"&k="+k;
   
       fetch(apiUrl)
         .then((res) => res.json())
-        .then((data) => setResponse(data)) // Set response data to state
+        .then((data) => 
+          {
+            const tmpImageArray = chunkArray(data,3);
+            setRImageArray(tmpImageArray)//update array
+            //alert(tmpImageArray[0][0].img_url)
+            //setResponse(data)
+         }) // Set response data to state
         .catch((error) => console.error('Error:', error));
     };
 
+
+ 
+
 return (
-<div>
-   <Form
+
+<Layout>
+  <Content style={siderStyle}>
+  <Form
     {...layout}
     name="nest-messages"
     onFinish={handleSubmit}
     style={{
-      maxWidth: 900,
+      maxWidth: '100%',
     }} 
   >
     <Form.Item name='desc'>
-      <Input.TextArea  rows={12} value={desc} onChange={(e) => setDesc(e.target.value)} placeholder='Describe your image of interest'/>
+      <Input.TextArea  rows={12} value={desc} onChange={(e) => setDesc(e.target.value)} placeholder='Describe your images of interest'/>
     </Form.Item>
     <Form.Item
       wrapperCol={{
@@ -46,20 +89,45 @@ return (
         offset: 8,
       }}
     >
+
+   <Form.Item name='k'>
+      <span>Number of matches (max:20) </span>
+      <InputNumber min={1} max={20} value={k} onChange={(value) => setK(value)}/>
+   </Form.Item>
+
+      
       <Button type="primary" htmlType="submit">
         Submit
       </Button>
     </Form.Item>
   </Form>
+  </Content>
+  <Sider width='50%' style={siderStyle}>
   <div id="api_results">
    {
-    response && (
-        <p>{response.message}</p>
+    rImageArray && (
+      <div style={{paddingLeft: 20, paddingRight: 10, maxHeight: '400px', overflowY: 'auto'}}>
+       {<table>
+        <tbody>
+          {rImageArray.map((imageRow, rowIndex) => (
+            <tr key={rowIndex}>
+              {imageRow.map((item, index) => (
+                <td key={index} style={{ width: '30%' }}>
+                  <img style={{ width: '100%' }} src={item.img_url} alt={`Image ${index + 1}`} />
+                  <br />
+                  Image {1+index + 3*(rowIndex)} - Score: {item.score}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+    </table>}
+      </div>
     )
    }
-   { /* results of API matching! */}
   </div>
-  </div>
+  </Sider>
+</Layout>
 )
 }
 
